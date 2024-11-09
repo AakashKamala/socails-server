021a8400f3419065bdc2418866059aa2c01d827c
Aakash <aakashkumar2908@gmail.com>
2024-11-09 16:42:39 +0530

    test1

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,9 @@ const express=require("express")
 const router=express.Router();
 
 router.route("/signup").post(signup)
+router.route("/signup").get((req, res)=>{
+    return res.json("jjj")
+})
 router.route("/login").post(login)
 
 module.exports=router
